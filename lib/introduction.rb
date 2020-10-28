b682e5d8a4e1fb8a334704d638b1dd7fe9658cd2
Brittani Baker <brittanibaker07@gmail.com>
2020-10-28 01:11:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,9 @@ def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
+def introduction_with_language_optional (name, language = "Ruby")
+  puts " HI, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 # Write your code here. If you get stuck, at the very bottom of this lab we've included
